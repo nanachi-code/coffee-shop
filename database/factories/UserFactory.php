d1359949810141b12e74dc6280009f8a8b0f19e7
sonthth1903012 <sonthth1903012@fpt.edu.vn>
2020-04-04 23:47:52 +0700

    update seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,12 +17,67 @@ use Illuminate\Support\Str;
 |
 */
 
-$factory->define(User::class, function (Faker $faker) {
+$factory->define(\App\Category::class,function (Faker $faker){
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token' => Str::random(10),
+        'category_name' => $faker->unique()->name
     ];
 });
+
+$factory->define(\App\Comment::class,function (Faker $faker){
+    return [
+        'post_id' => random(1,3),
+        'user_id' => random(1,3),
+        'content' => $faker->unique()->paragraph,
+    ];
+});
+$factory->define(\App\Order::class,function (Faker $faker){
+    return [
+        'customer_address' => $faker->unique()->address,
+        'customer_city' => $faker->unique()->city,
+        'customer_country' => $faker->unique()->country,
+        'customer_email'   =>  $faker->unique()->email,
+        'customer_name' => $faker->unique()->name,
+        'customer_phone'=>  $faker->unique()->phoneNumber,
+        'customer_postcode'=> $faker->unique()->postcode,
+        'method'=> random_int(1,5),
+        'status'=> random(1,5),
+        'total'=> random_int(1,5),
+    ];
+});
+
+
+$factory->define(\App\Post::class,function (Faker $faker){
+    return [
+        'user_id' => random(1,3),
+        'content' => $faker->unique()->paragraph,
+        'title' => $faker->unique()->title,
+    ];
+});
+
+$factory->define(\App\Product::class,function (Faker $faker){
+    return [
+        'price_id' => random(1,3),
+        'desc' => $faker->unique()->paragraph,
+        'name' => $faker->unique()->name,
+        'stock' => random(20,99),
+    ];
+});
+
+$factory->define(\App\Post::class,function (Faker $faker){
+    return [
+        'user_id' => random(1,3),
+        'content' => $faker->unique()->paragraph,
+        'title' => $faker->unique()->title,
+    ];
+});
+
+$factory->define(\App\User::class,function (Faker $faker){
+    return [
+
+    ];
+});
+
+
+
+
+
