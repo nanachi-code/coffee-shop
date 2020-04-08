5a734cbb5e26ca222619d2e8dcd0d8127484b4e5
jklhdd <49911153+jklhdd@users.noreply.github.com>
2020-04-08 14:31:51 +0700

    Update footer.blade.php

--- resources/views/html/footer.blade.php
+++ resources/views/html/footer.blade.php
@@ -17,7 +17,8 @@
             <div class="col-lg-4 col-md-6 mb-5 mb-md-5">
                 <div class="ftco-footer-widget mb-4">
                     <h2 class="ftco-heading-2">Recent Blog</h2>
-                    @if (isset($data))
+                    @isset($data)
+                    @empty($data)
                     @for ($i = 0; $i < 2; $i++) @php $p=$data['recent_blog']->get($i); @endphp <div
                             class="block-21 mb-4 d-flex">
                             <a class="blog-img mr-4" style="background-image: url({{asset($p->thumbnail)}});"></a>
@@ -32,9 +33,8 @@
                             </div>
                         </div>
                         @endfor
-
-                        @endif
-
+                        @endempty
+                        @endisset
 
                 </div>
             </div>
